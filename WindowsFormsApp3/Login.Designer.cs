c27e3802120544ce99246e5efc56432c03415988
Mario <meridiano1998@hotmail.com>
2019-09-12 10:21:13 -0500

    primercambodaniel

--- WindowsFormsApp3/Login.Designer.cs
+++ WindowsFormsApp3/Login.Designer.cs
@@ -82,9 +82,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(21, 172);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.Size = new System.Drawing.Size(99, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "PRUEBA_la maria";
+            this.label2.Text = "PRUEBA_la maria_";
             // 
             // label1
             // 
